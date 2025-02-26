752382cee7298c275f72a8a1c8f18c41790c2c72
jrdurrcodes <biycoachn@yahoo.com>
2025-02-26 14:40:32 -0600

    created form for movie tickets

---
+++ js/app.js
@@ -0,0 +1,58 @@
+/**
+ * select a movie
+ * get # of tickets
+ * multiply # of tickets by ticket price
+ */
+
+let data = {
+    movie: '',
+    totalTickets: 0,
+    price: 10.79,
+    total: 0
+}
+
+const toggleDisplay = document.getElementById('toggleDisplay')
+let tickets = 0
+
+toggleDisplay.innerText = tickets
+
+const submitBtn = document.getElementById('submitBtn')
+
+submitBtn.addEventListener('click', (e) => {
+    console.log('click')
+} )
+
+const updateData =()=> {
+    const movie = document.querySelector('select{name="movie"]').value
+
+    const totalTickets = parseInt(document.getElementById('totalTickets').value)
+    
+    data.movie = movie
+    data.totalTickets = totalTickets
+    data.total = data.totalTickets * data.price
+
+    return data
+}
+
+const getTicketAmt = (id)=> {
+
+
+    
+    if (id == 'add') {
+        tickets++
+    }else if (id == 'subtract' && tickets > 0) {
+        tickets
+    }
+
+    return tickets
+    }
+
+    const togglers = document.querySelectorAll('toggler')
+
+    togglers.forEach(toggler => {
+        toggler.addEventListener('click', ()=> {
+            getTicketAmt(toggler.id)
+        })
+    })
+
+
